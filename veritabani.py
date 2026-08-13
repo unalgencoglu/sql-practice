22ba9744c1ec8fd578a082eadfa3660848d2ffaf
Unal Gencoglu <unal.gencoglu39@gmail.com>
2026-08-13 11:07:11 +0300

    kitapları listeleyen metod yazıldı

--- veritabani.py
+++ veritabani.py
@@ -9,6 +9,7 @@ def baglan():
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
 
 def kitap_ekle(ad, yazar, sayfa):
@@ -19,9 +20,22 @@ def kitap_ekle(ad, yazar, sayfa):
             """
             INSERT INTO kitaplar (ad, yazar, sayfa)
             VALUES (%s, %s, %s)
-            """
+            """,
             (ad, yazar, sayfa))
     baglanti.commit()
     cursor.close()
     baglanti.close()
     
+def kitaplari_listele():
+    baglanti = baglan()
+    cursor = baglanti.cursor()
+    
+    cursor.execute(
+            """
+            SELECT * FROM kitaplar
+            """,
+    )
+    sonuc = cursor.fetchall()
+    cursor.close()
+    baglanti.close()
+    return sonuc
